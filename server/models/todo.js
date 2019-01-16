9bc06588cfcf827aeaba9b935eb0206303ae441f
martina faragalla <martinamaken@gmail.com>
2019-01-16 12:59:11 -0800

    add mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+const Todo = mongoose.model('Todo',{
+  text:{
+    type:String
+  },
+  completed :{
+    type:Boolean
+  },
+  completedAt: {
+    type:Number
+  }
+});
+module.exports = {Todo};
---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+const User = mongoose.model('user',{
+  email : {
+    type:String,
+    require:true,
+    trim : true,
+    minlength:1
+  }
+})
+module.exports = {User};
